f56f61e1ae9abc878a98bab15f249e03b72fa4ef
Shrey <shhr3y@gmail.com>
2020-08-31 12:41:26 +0530

    Initial Commit

---
+++ WallPaint/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallPaint
+//
+//  Created by Shrey Gupta on 31/08/20.
+//  Copyright © 2020 Shrey Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
